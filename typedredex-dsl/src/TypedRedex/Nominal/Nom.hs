d0307915a6d2d1d2d95b559ee6a7f6ddb673aa9a
Xu Xue <juniorxxue@gmail.com>
2026-01-22 23:20:03 +0800

    huge refactor: standardize freedeep project, and remove others

--- typedredex-dsl/src/TypedRedex/Nominal/Nom.hs
+++
@@ -1,9 +0,0 @@
--- | Nominal atom typeclass for lambda binding.
---
--- This module re-exports 'NominalAtom' from the logic layer.
--- For standard name types ('Nom', 'TyNom'), see "TypedRedex.Nominal.Prelude".
-module TypedRedex.Nominal.Nom
-  ( NominalAtom(..)
-  ) where
-
-import TypedRedex.Logic.Nominal (NominalAtom(..))
